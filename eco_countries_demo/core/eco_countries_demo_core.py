62d52ebf3178392152b4918d992e6b8bd9904d34
Kalimaha <guido.barbaglia@gmail.com>
2015-06-12 17:33:35 +0200

    Class created.

--- eco_countries_demo/core/eco_countries_demo_core.py
+++ eco_countries_demo/core/eco_countries_demo_core.py
@@ -4,26 +4,42 @@ from geobricks_downloader.core.downloader_core import Downloader
 from eco_countries_demo.config.processing_config import processing
 
 
-layers = c.list_layers_countries_subset('MYD11C1', '2015', '001', 'af')
-my_downloader = Downloader('modis',
-                           '/home/kalimaha/Desktop',
-                           {'product': 'MYD11C1', 'year': '2015', 'day': '001'},
-                           layers,
-                           True)
-downloaded_layers = my_downloader.download()['downloaded_files']
-processing[0]['source_path'] = ['/home/kalimaha/Desktop/MYD11C1/2015/001/' + layers[0]['file_name']]
-processing[0]['output_path'] = '/home/kalimaha/Desktop/MYD11C1/2015/001/PROCESSED/'
-download_in_progress = True
-while download_in_progress:
-    try:
-        current = my_downloader.progress(layers[0]['file_name'])['download_size']
-        total = my_downloader.progress(layers[0]['file_name'])['total_size']
-        download_in_progress = current != total
-    except TypeError, e:
-        pass
-    except KeyError:
-        pass
-print 'Layer downloaded.'
-print 'Start processing...'
-processed_files = processing_core.process_data(processing)
-print 'Processing done.'
+class ECOCountriesDownloader:
+
+    products = ['MYD11C1']
+    years = ['2015', '2014']
+    countries = 'af,az,ir,kz,kg,pk,tj,tr,tm,uz'
+
+    def __init__(self):
+        for p in self.products:
+            for y in self.years:
+                days = c.list_days(p, y)
+                for d in days:
+                    layers = c.list_layers_countries_subset(p, y, d['code'], self.countries)
+                    my_downloader = Downloader('modis',
+                                               '/home/kalimaha/Desktop',
+                                               {'product': p, 'year': y, 'day': d['code']},
+                                               layers,
+                                               True)
+                    downloaded_layers = my_downloader.download()['downloaded_files']
+                    processing[0]['source_path'] = ['/home/kalimaha/Desktop/' + p + '/' + y + '/' + d['code'] + '/' + layers[0]['file_name']]
+                    processing[0]['output_path'] = '/home/kalimaha/Desktop/' + p + '/' + y + '/' + d['code'] + '/PROCESSED/'
+                    download_in_progress = True
+                    while download_in_progress:
+                        try:
+                            current = my_downloader.progress(layers[0]['file_name'])['download_size']
+                            total = my_downloader.progress(layers[0]['file_name'])['total_size']
+                            download_in_progress = current != total
+                        except TypeError, e:
+                            pass
+                        except KeyError:
+                            pass
+                    print 'Layer downloaded.'
+                    print 'Start processing...'
+                    processed_files = processing_core.process_data(processing)
+                    print processed_files
+                    print 'Processing done.'
+
+
+
+dwld = ECOCountriesDownloader()
