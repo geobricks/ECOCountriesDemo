3c45c7a8c2482398ce6d61b81593d1877a3a7026
simonefenix <simone.murzilli@fao.org>
2015-10-22 11:17:43 +0200

    added new calculations

--- eco_countries_demo/processing/anomaly_dpy_rasterio_diff.py
+++ eco_countries_demo/processing/anomaly_dpy_rasterio_diff.py
@@ -24,33 +24,36 @@ def calc(basepath, output_path, layers, epsg="3857"):
 
         print "Processing: ", layer, layerPrev
 
-        print "Reading: ",  layer
-        r = rasterio.open(layer)
-        r_data = r.read_band(1).astype(float)
+        try:
+            print "Reading: ",  layer
+            r = rasterio.open(layer)
+            r_data = r.read_band(1).astype(float)
 
-        print "Reading: ",  layerPrev
-        r_prev = rasterio.open(layerPrev)
-        r_prev_data = r_prev.read_band(1).astype(float)
+            print "Reading: ",  layerPrev
+            r_prev = rasterio.open(layerPrev)
+            r_prev_data = r_prev.read_band(1).astype(float)
 
-        if data is None:
-            data, kwargs = initialize_rasterio_raster(r, rasterio.float32)
+            if data is None:
+                data, kwargs = initialize_rasterio_raster(r, rasterio.float32)
 
 
-        data = r_data - r_prev_data
+            data = r_data - r_prev_data
 
-        # writing
-        output_layer_path = output_path + "/" + filename + ".tif"
-        print "Writing: ", output_layer_path
-        with rasterio.open(output_layer_path, 'w', **kwargs) as dst:
-            dst.write_band(1, data.astype(rasterio.float32))
+            # writing
+            output_layer_path = output_path + "/" + filename + ".tif"
+            print "Writing: ", output_layer_path
+            with rasterio.open(output_layer_path, 'w', **kwargs) as dst:
+                dst.write_band(1, data.astype(rasterio.float32))
+        except Exception, e:
+            print e
 
 
 
 def process_all():
-    # basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/MYD11C3"
-    # output_path = basepath + "/MYD11C3_ANOMALY_DPY"
-    # layers = glob.glob(basepath + "/*.tif")
-    # calc(basepath, output_path, layers)
+    basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/MYD11C3"
+    output_path = basepath + "/MYD11C3_ANOMALY_DPY"
+    layers = glob.glob(basepath + "/*.tif")
+    calc(basepath, output_path, layers)
 
     #
     # basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/MOD16/ET/ET"
@@ -58,10 +61,10 @@ def process_all():
     # layers = glob.glob(basepath + "/*.tif")
     # calc(basepath, output_path, layers)
 
-    basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/CHIRPS"
-    output_path = basepath + "/anomalies_dpy/"
-    layers = glob.glob(basepath + "/*.tif")
-    calc(basepath, output_path, layers)
+    # basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/CHIRPS"
+    # output_path = basepath + "/anomalies_dpy/"
+    # layers = glob.glob(basepath + "/*.tif")
+    # calc(basepath, output_path, layers)
 
     # basepath = "/media/vortex/LaCie/LaCie/ECO_COUNTRIES/MOD13A3"
     # output_path = basepath + "/MOD13A3_ANOMALY_DPY/original"
